6b42101d874346a4e098416f7cd328ac61fb9933
ELHALOUI <mohammed.elhaloui.se@gmail.com>
2018-10-09 07:02:18 +0100

    update gitignore

--- build.sbt
+++ build.sbt
@@ -9,5 +9,10 @@ lazy val akkaVersion = "2.5.16"
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % akkaVersion,
   "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
-  "org.scalatest" %% "scalatest" % "3.0.5" % "test"
+  "com.typesafe.akka" %% "akka-persistence" % akkaVersion,
+  "com.typesafe.akka" %% "akka-persistence-query" % akkaVersion,
+  "org.iq80.leveldb" % "leveldb" % "0.7",
+  "org.fusesource.leveldbjni" % "leveldbjni-all" % "1.8",
+  "joda-time" % "joda-time" % "2.10",
+"org.scalatest" %% "scalatest" % "3.0.5" % "test"
 )
--- src/main/scala/com/example/supervisor.scala
+++ src/main/scala/com/example/supervisor.scala
@@ -3,32 +3,53 @@ package com.example
 import akka.actor._
 import akka.pattern._
 import akka.testkit.TestActors.EchoActor
-import scala.io.Source
 import akka.actor.SupervisorStrategy._
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
+import akka.persistence._
+import akka.persistence.query._
+import org.joda.time.DateTime
 
+class Supervisor extends PersistentActor with ActorLogging {
 
-class Supervisor extends Actor with ActorLogging {
+  case class LastBatchTime(time : Long)
 
+  override def persistenceId: String = "test-persistent-actor-counter"
   var batchActors : Map[String, ActorRef] = Map.empty[String, ActorRef] // a map containing the ActorRef of the actor that executing the batchName
 
   override def preStart(): Unit = log.info(s"Supervisor is Ready")
   override def preRestart(reason: Throwable, message: Option[Any]): Unit = log.info(s"Supervisor restarting...   --> ${reason}")
   override def postStop() = log.info(s"Supervisor Stopped")
+  
+
+  context.system.scheduler.schedule(
+    30 milliseconds,
+    5 seconds){
+    //self  ! StartBatchs
+    println(s"\n scheduler \n")
+  }
 
 
+  override def receiveRecover: Receive = {
+    case time : Long =>   println(s"\n time ==> ${time}\n")
+    case _ =>   println
 
-  def receive = {
+  }
+
+  override def receiveCommand = {
     case EndJob(batchName) =>  {
-      println(s"\n\n\n\n batchName ==> ${batchName}\n\n\n\n")
+      println(s"\n batchName ==> ${batchName}\n")
       batchActors -= batchName
       println(batchActors)
     }
     case StartBatchs => {
-       val batch1Props = BatchActor.apply(self)
-       startBatch(BatchActor.batchName , batch1Props).map(batchActors += BatchActor.batchName -> _)
-       println(batchActors)
+       persist(new DateTime().getMillis() / 1000 ){ lbt =>
+         println(s"\nlbt ==> ${lbt}\n")
+         val batch1Props = BatchActor(self)
+         startBatch(BatchActor.batchName , batch1Props).map(batchActors += BatchActor.batchName -> _)
+         println(batchActors)
+       }
+
      }
     case _ => println
 
